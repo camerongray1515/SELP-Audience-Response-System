71103f40c5bc55e897c61f8a02c247e41ccfbb47
Cameron Gray <cameron@camerongray.me>
2014-09-25 22:14:07 +0100

    Enabled Django admin for all of the models to allow management of the database before the proper management pages are built

--- flash_response/main/admin.py
+++ flash_response/main/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Tutor)
+class Tutor_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Student)
+class Student_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Course)
+class Course_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tutor_assignment)
+class Tutor_assignment_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Enrollment)
+class Enrollment_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Session)
+class Session_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class Question_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question_option)
+class Question_option_admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Student_response)
+class Student_response_admin(admin.ModelAdmin):
+    pass
