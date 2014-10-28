d176a48eaf8848d44311d1e88ccafdb047afc17c
Cameron Gray <cameron@camerongray.me>
2014-10-28 20:59:05 +0000

    Question is now displayed to student at correct time

--- flash_response/assets/js/student.js
+++ flash_response/assets/js/student.js
@@ -6,8 +6,9 @@ function RunningSesson(sessionCode) {
             'session_code': this.sessionCode
         }, function(response) {
             if (response.question_available) {
-                console.log("Question Available");
-                setTimeout(showQuestion, response.time_to_start);
+                setTimeout(function() {
+                    showQuestion(response);
+                }, response.time_to_start);
             } else {
                 // If there were no questions, check again in 2 seconds
                 setTimeout(runningSession.checkForQuestions, 2000);
@@ -15,8 +16,19 @@ function RunningSesson(sessionCode) {
         });
     };
 
-    showQuestion = function() {
-        console.log("Question Started!");
+    showQuestion = function(question) {
+        $('#question-title').text(question.question_body);
+        var question_option_template = $('#template-question-option').text();
+        for (var i = question.question_options.length - 1; i >= 0; i--) {
+            option = question.question_options[i];
+            option_html = question_option_template;
+            option_html = option_html.replace(/\[\# option_id \#\]/g, option.id);
+            option_html = option_html.replace(/\[\# option_body \#\]/g, option.body);
+            $('#question-option-container').append(option_html);
+        };
+        $('#wait-container').fadeOut(function() {
+            $('#question-container').fadeIn();
+        });
     };
 }
 
