30798bef36c3f7353847a84b61609b1f58c7434d
Cameron Gray <cameron@camerongray.me>
2014-10-31 18:22:45 +0000

    Input validation at response collection backednd complete.  Still need to insert response

--- flash_response/student/urls.py
+++ flash_response/student/urls.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^check_question_availability/$', 'student.views.check_question_availability'),
+    url(r'^log_response/$', 'student.views.log_response'),
 )
--- flash_response/student/views.py
+++ flash_response/student/views.py
@@ -57,3 +57,41 @@ def check_question_availability(request):
             data['run_time'] = newest_assignment.run_time
 
     return HttpResponse(json.dumps(data), content_type='application/json')
+
+@user_is_student
+def log_response(request):
+    if request.method != 'GET':
+        raise Http404
+
+    session_code = request.GET.get('session_code')
+    option_id = request.GET.get('option_id')
+
+    data = {'success': True, 'message': 'Response has been collected successfully'}
+
+    # We must now do some strict checking to ensure that the user is allowed to
+    # answer this question and if all that passes, we insert the response
+    try:
+        # User must be allowed to access this session and session must be active
+        session = Session.objects.get(url_code=session_code, course__enrollment__student__user=request.user, running=True)
+        # This option must be part of the current and the question must be active
+        option = Question_option.objects.get(pk=option_id)
+        current_question = Current_question.objects.filter(session=session, question=option.question).order_by('-start_time')
+        if not current_question:
+            raise ObjectDoesNotExist
+    except ObjectDoesNotExist:
+        data['success'] = False
+        data['message'] = 'You do not have permission to respond to this session with that option'
+
+    
+    if data['success']:
+        # Now check that the current question is still running
+        newest_assignment = current_question[0]
+        end_time = newest_assignment.start_time+datetime.timedelta(0, newest_assignment.run_time)
+        if timezone.now() > end_time:
+            data['success'] = False
+            data['message'] = 'This question has expired'
+        else:
+            pass
+            # Insert the response
+
+    return HttpResponse(json.dumps(data), content_type='application/json')
