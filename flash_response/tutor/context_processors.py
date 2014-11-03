b9e7706fbd33848461b0d72ac9a5456e0dc2f537
Cameron Gray <cameron@camerongray.me>
2014-11-03 14:43:30 +0000

    Removed old tutor check from context processor

--- flash_response/tutor/context_processors.py
+++ flash_response/tutor/context_processors.py
@@ -1,16 +1,9 @@
-from main.models import Tutor_assignment, Tutor
+from main.models import Tutor_assignment
 from django.core.exceptions import ObjectDoesNotExist
 
-def course_assignments_processor(request):
-    tutor = None
-    if request.user.is_authenticated:
-        try:
-            tutor = Tutor.objects.get(user=request.user.pk) # Get the logged in tutor
-        except ObjectDoesNotExist:
-            pass
-            
+def course_assignments_processor(request):            
     if tutor:
-        course_assignments = Tutor_assignment.objects.filter(tutor=tutor) # Get all course assignments
+        course_assignments = Tutor_assignment.objects.filter(user=request.user) # Get all course assignments
     else:
         course_assignments = []
 
