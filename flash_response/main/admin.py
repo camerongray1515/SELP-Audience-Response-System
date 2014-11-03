5379e1132704252a038702c168424a3eb4b32b8b
Cameron Gray <cameron@camerongray.me>
2014-11-03 13:59:43 +0000

    Unregistered removed models from admin area

--- flash_response/main/admin.py
+++ flash_response/main/admin.py
@@ -1,24 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-@admin.register(Tutor)
-class Tutor_admin(admin.ModelAdmin):
-    pass
-
-@admin.register(Student)
-class Student_admin(admin.ModelAdmin):
-    pass
-
 @admin.register(Course)
 class Course_admin(admin.ModelAdmin):
     pass
 
 @admin.register(Tutor_assignment)
 class Tutor_assignment_admin(admin.ModelAdmin):
-    pass
-
-@admin.register(Enrollment)
-class Enrollment_admin(admin.ModelAdmin):
     pass
 
 @admin.register(Session)
