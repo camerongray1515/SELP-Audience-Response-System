6b1f5adb94df37eeae6dcf23d45bec96b939f018
Cameron Gray <cameron@camerongray.me>
2014-11-03 14:44:57 +0000

    Fixed tutor context processor to now check user instead of tutor object (which doesn't exist)

--- flash_response/tutor/context_processors.py
+++ flash_response/tutor/context_processors.py
@@ -2,8 +2,8 @@ from main.models import Tutor_assignment
 from django.core.exceptions import ObjectDoesNotExist
 
 def course_assignments_processor(request):            
-    if tutor:
-        course_assignments = Tutor_assignment.objects.filter(user=request.user) # Get all course assignments
+    if request.user:
+        course_assignments = Tutor_assignment.objects.filter(user=request.user.id) # Get all course assignments
     else:
         course_assignments = []
 
