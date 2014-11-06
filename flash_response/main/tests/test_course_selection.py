56579c00d8a0c8c56b1de1d0e1f97d9d906ba731
Cameron Gray <cameron@camerongray.me>
2014-11-06 19:38:28 +0000

    Added more course selection tests to check courses that exist, don't exist and that the tutor is not assigned to

--- flash_response/main/tests/test_course_selection.py
+++ flash_response/main/tests/test_course_selection.py
@@ -15,3 +15,26 @@ class TestCourseSelection(TestCase):
         response = self.client.get('/tutor/')
         
         self.assertContains(response, 'you must select a course')
+
+    # "Select a course" message should be displayed again after attempting to
+    # select a course that does not exist
+    def test_select_nonexistant_course(self):
+        self.makeNewClient()
+        response = self.client.post('/tutor/select_course/', {'course': -1}, follow=True)
+        
+        self.assertContains(response, 'you must select a course')
+
+    # "Select a course" message should be displayed again after attempting to
+    # select a course is not assigned to the logged in user
+    def test_select_unassigned_course(self):
+        self.makeNewClient()
+        response = self.client.post('/tutor/select_course/', {'course': 2}, follow=True)
+        
+        self.assertContains(response, 'you must select a course')
+
+    # "Select a course" message disappear after selecting a course that the tutor is assigned to
+    def test_select_assigned_course(self):
+        self.makeNewClient()
+        response = self.client.post('/tutor/select_course/', {'course': 1}, follow=True)
+        
+        self.assertNotContains(response, 'you must select a course')
