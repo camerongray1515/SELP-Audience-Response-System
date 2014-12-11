e9dfe02960d7dc9c104141c4fee3d221f92987f8
Cameron Gray <cameron@camerongray.me>
2014-12-11 16:26:52 +0000

    Student frontend now sends a UUID string to the server to "practically uniquely" identify each responder.  This will be used to count how many students are currently waiting to respond to questions.

--- flash_response/assets/js/student.js
+++ flash_response/assets/js/student.js
@@ -1,9 +1,10 @@
 function RunningSesson(sessionCode) {
     this.sessionCode = sessionCode;
 
-    this.checkForQuestions = function() {
+    this.checkForQuestions = function(sessionCode, responderUUID) {
         $.get('/student/check_question_availability/', {
-            'session_code': this.sessionCode
+            'session_code': sessionCode,
+            'responder_uuid': responderUUID
         }, function(response) {
             if (response.question_available) {
                 setTimeout(function() {
@@ -11,7 +12,7 @@ function RunningSesson(sessionCode) {
                 }, response.time_to_start);
             } else {
                 // If there were no questions, check again in 2 seconds
-                setTimeout(runningSession.checkForQuestions, 2000);
+                setTimeout(function() {runningSession.checkForQuestions(sessionCode, responderUUID)}, 2000);
             }
         });
     };
@@ -92,6 +93,23 @@ function RunningSesson(sessionCode) {
         });
     };
 
+    // createUUID function taken from - http://stackoverflow.com/a/873856
+    createUUID = function() {
+        // http://www.ietf.org/rfc/rfc4122.txt
+        var s = [];
+        var hexDigits = "0123456789abcdef";
+        for (var i = 0; i < 36; i++) {
+            s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+        }
+        s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+        s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+        s[8] = s[13] = s[18] = s[23] = "-";
+
+        var uuid = s.join("");
+        return uuid;
+    }
+    this.responderUUID = createUUID(); // This is used to identify the responder
+
     // Binding for question options being clicked
     $(document).on('click', '.question-option-button', function() {
         uiToResponseTransmitted();
@@ -100,7 +118,7 @@ function RunningSesson(sessionCode) {
         transmitResponse(option_id);
     });
 
-    this.checkForQuestions();
+    this.checkForQuestions(this.sessionCode, this.responderUUID);
 }
 
 var runningSession = new RunningSesson(sessionCode);
